e97980573c5991a3a16eafd7d1280aad0b6641e6
Ismail Cherri <ismailcherri@gmail.com>
2026-07-01 20:44:13 +0200

    feat: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -29,7 +29,7 @@ export default tseslint.config(
       },
     },
     settings: {
-      react: { version: 'detect' },
+      react: { version: '19.2.7' },
     },
     rules: {
       ...react.configs.recommended.rules,
